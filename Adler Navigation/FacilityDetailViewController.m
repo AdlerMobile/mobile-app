d41829288f63bb35df1d77ea7dd8429b764c5f75
Smit Patel <patel176@illinois.edu>
2014-05-04 18:16:49 -0500

    1. added custom button class 2. changed font for the segment titles on the Info tab 3. changed the image size for the exhibits tab. increased text view height and reduced the height of the exhibit image

--- Adler Navigation/FacilityDetailViewController.m
+++ Adler Navigation/FacilityDetailViewController.m
@@ -20,20 +20,11 @@
 {
     [super viewDidLoad];
     [_navBar setTitle:_data];
-    _navButton.layer.borderWidth = 1.5f;
-    _navButton.layer.borderColor = [[UIColor blackColor] CGColor];
-    _navButton.layer.cornerRadius = 10.0f;
-    //self.view.backgroundColor = [UIColor blackColor];
-    //_textDescription.backgroundColor = [UIColor blackColor];
-    //_textDescription.textColor = [UIColor whiteColor];
     if ([_data isEqualToString:@"Café Galileo's"]) {
         _textDescription.text = @"For a nice, relaxing lunch and the best view of the Chicago Skyline there's nowhere better than Café Galileo’s. Café Galileo’s provides a variety of freshly prepared options, including a large selection of beverages, soups, sandwiches, salads and desserts.";
         _image.image = [UIImage imageNamed:@"cafe.jpg"];
         _onlineStoreButton.hidden = YES;
         _viewMenuButton.hidden = NO;
-        _viewMenuButton.layer.borderWidth = 1.5f;
-        _viewMenuButton.layer.borderColor = [[UIColor blackColor] CGColor];
-        _viewMenuButton.layer.cornerRadius = 10.0f;
     }
     
     if ([_data isEqualToString:@"Adler Store"]) {
@@ -41,10 +32,6 @@
         _image.image = [UIImage imageNamed:@"store.jpg"];
         _viewMenuButton.hidden = YES;
         _onlineStoreButton.hidden = NO;
-        _onlineStoreButton.layer.borderWidth = 1.5f;
-        _onlineStoreButton.layer.borderColor = [[UIColor blackColor] CGColor];
-        _onlineStoreButton.layer.cornerRadius = 10.0f;
-        
     }
     
     // Do any additional setup after loading the view.
